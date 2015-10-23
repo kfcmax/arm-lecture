6e73955864ab221dfeceb878819edac21fb83928
kfcmax <kfcmax@hotmail.com>
2015-10-23 11:29:11 +0800

    Add Fibonacci Non-Recursive use assembly lanuage.
    use non-recursive (loop)

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,31 +13,20 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r3, #-1 		@ r3 = previous
+    	mov r4, #1 		@ r4 = result
+   	mov r5, #0 		@ r5 = sum
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+.loop:
+	 add r5, r3, r4		@ sum = previous + result
+	 mov r3, r4 		@ previous = result
+	 mov r4, r5 		@ result = sum
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	 subs r0, r0, #1 		@ if r0 >= 0 , r0 = r0 - 1   
+	 bge .loop 
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-	.size fibonacci, .-fibonacci
-	.end
+	 mov r0, r4 		@ r0 = result
+	 pop {r3, r4, r5, pc}
+	    
+.size fibonacci, .-fibonacci
+ .end    
